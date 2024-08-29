29b60c8b18a10da56c4f773db71186ec0cbe122b
myw4072 <myw4072@rit.edu>
2024-08-29 19:50:18 -0400

    sdf

---
+++ wang-quotes-app/src/index.js
@@ -0,0 +1,63 @@
+// HELPERS
+// NOTE: You can test this in the browser console
+const randomElement = array => {
+  if(!array.length) throw new Error("Array is empty!");
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+// NOTE: How about implementing const getJsonXHR = (url, callback) => {...}
+const getJsonXHR = (url, callback) => {
+  const xhr = new XMLHttpRequest();
+  xhr.onload = () => {
+    if(xhr.status == "404"){
+      console.log(`NOT FOUND: ${url}`);
+      callback([{content: `NOT FOUND: ${url}`}]);
+      return;
+    }
+    const text = xhr.responseText;
+    
+    let json;
+    try{
+      json = JSON.parse(text);
+    }
+    catch(err){
+      console.log(`ERROR: ${err}`);
+      json = [{content: `Don't take any wooden nickels!`}];
+    }
+    finally{
+      callback(json);
+    }
+
+    
+    callback(json);
+  }
+  xhr.open("GET", url);
+  //add headers here (send request headers?)
+  xhr.send();
+}
+// MAIN
+// Note: The page is already loaded (that's what <script defer ...> does) 
+// ... so go to town!
+// ... get a reference to "random" button 
+// ... get a reference to "results" <div>
+// ... and so on
+
+const button = document.querySelector("#btn-random");
+const resultDiv = document.querySelector("#results");
+
+const resultAuthor = document.querySelector("#author");
+const resultQuote = document.querySelector("#quote");
+
+const jsonUrl = "data/quotes-data.json";
+
+const quoteComponent = json => {
+  //resultDiv.innerHTML = `"<i>${randomElement(json).content}</i>"" <b>- ${randomElement(json).author}</b>`;
+
+  resultAuthor.innerHTML = randomElement(json).author;
+  resultQuote.innerHTML = `"<i>${randomElement(json).content}</i>"`;
+};
+
+button.onclick = () => {
+  //console.log("button clicked");
+  getJsonXHR(jsonUrl, quoteComponent);
+};
